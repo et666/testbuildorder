1bb9830570298a6d5ac6dbff1d7933ccf5da8a0d
et666 <et666@Adrians-MBP.fritz.box>
2018-05-31 17:55:39 +0200

    Added: killserver

---
+++ killServer.py
@@ -0,0 +1,50 @@
+
+#coding: utf-8
+from bottle import route, error, post, get, run, static_file, abort, redirect, response, request, template
+import subprocess
+
+@route('/')
+@route('/startProcess/<processName>')
+def startProcess(processName):
+    result = []
+    process = subprocess.Popen(processName,
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    for line in process.stdout:
+        result.append(line)
+    
+    errcode = process.returncode
+   
+    if errcode is not None:
+        return 'FAILED to start: ' + processName + ' /n ' + result
+
+    return 'Start Process: ' + processName + ' /n ' + result
+
+@route('/killProcess/<processName>')
+def killProcess(processName):
+    handle = subprocess.Popen(processName, shell=False)
+    subprocess.Popen("taskkill /F /T /PID %i"%handle.pid , shell=True)
+
+    return 'Tried to kill: ' + processName
+
+@route('/isProcessActive/<processName>')
+def killProcess(processName):
+    tlcall = 'TASKLIST', '/FI', 'imagename eq %s' % processName
+    # shell=True hides the shell window, stdout to PIPE enables
+    # communicate() to get the tasklist command result
+    tlproc = subprocess.Popen(tlcall, shell=True, stdout=subprocess.PIPE)
+    # trimming it to the actual lines with information
+    tlout = tlproc.communicate()[0].strip().split('\r\n')
+    # if TASKLIST returns single line without processname: it's not running
+    if len(tlout) > 1 and processName in tlout[-1]:
+        return ('process "%s" is running!' % processName)
+    else:
+        print(tlout[0])
+        return ('process "%s" is NOT running!' % processName)
+
+@error(404)
+def error404(error):
+    return '404 error !!!!!'
+
+run(host='localhost', port=8002, reloader=True)
