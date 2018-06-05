8675c1faa758963bc784f931666d6a2afd21f11d
et666 <et666@Adrians-MBP.fritz.box>
2018-06-05 19:53:12 +0200

    Changed: Test

--- comparer.py
+++ comparer.py
@@ -1,19 +1,55 @@
 
 import sys
 
-args = sys.argv
-print args
+def checkout(component):
+    print 'checkout'
 
-file1 = open(args[1])
-lines1 = [line.rstrip('\n') for line in file1]
+def change(component):
+    print 'change' 
 
-file2 = open(args[2])
-lines2 = [line.rstrip('\n') for line in file2]
+def tag(component):
+    print 'tag' 
 
-for line in lines1:
-    if line.startswith('-'):
-        print line
-        continue
+def push(component):
+    print 'push' 
 
-    if line not in lines2:
-        print line        
+def cleanFolder():
+    print 'clean Folder'
+
+def main():
+    args = sys.argv
+
+    file1 = open(args[1])
+    lines1 = [line.rstrip('\n') for line in file1]
+
+    file2 = open(args[2])
+    lines2 = [line.rstrip('\n') for line in file2]
+
+    count = 0
+    buildOrder = []
+    for line in lines1:
+        if line.startswith('-'):
+            print line
+            count+= 1
+            buildOrder.append([])
+            continue
+
+        if line not in lines2:
+            print line
+            buildOrder[count-1].append(line)
+
+    buildOrder = filter(None, buildOrder)
+
+    for buildStage in buildOrder:
+        cleanFolder()
+        print buildOrder.index(buildStage)
+        for component in buildStage:
+            checkout(component)
+            change(component)
+            tag(component)
+            push(component)
+
+        raw_input("Press Enter to continue...")
+
+if __name__ == "__main__":
+    main()
